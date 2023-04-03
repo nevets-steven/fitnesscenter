e3e22d0b6798362628246c746b0400fd3da613dd
nevets-steven <nevets322@gmail.com>
2023-04-03 19:39:53 -0400

    moved andrew code from main.py -> fitness.py

--- fitnesscenter.py
+++ fitnesscenter.py
@@ -37,3 +37,62 @@ class MultiClubMember(Member):
         print(f"Welcome {self.name} to {club.name}! Membership points: {self.membership_points}")
         return True
 
+class Club:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+class FitnessCenter:
+    def __init__(self):
+        self.members = []
+
+    def add_member(self):
+        member_id = input("Enter member ID: ")
+        name = input("Enter name: ")
+        membership_type = input("Enter membership type (s for Single Club, m for Multi-Club): ")
+        if membership_type == "s":
+            club_name = input("Enter club name: ")
+            club_address = input("Enter club address: ")
+            club = Club(club_name, club_address)
+            member = SingleClubMember(member_id, name, club)
+        elif membership_type == "m":
+            member = MultiClubMember(member_id, name)
+        else:
+            print("Error: Invalid membership type.")
+            return
+        self.members.append(member)
+        print(f"Member {member.name} ({member.member_id}) has been added.")
+
+    def remove_member(self):
+        member_id = input("Enter member ID: ")
+        for member in self.members:
+            if member.member_id == member_id:
+                self.members.remove(member)
+                print(f"Member {member.name} ({member.member_id}) has been removed.")
+                return
+        print(f"Error: Member {member_id} not found.")
+
+    def display_member_info(self):
+        member_id = input("Enter member ID: ")
+        for member in self.members:
+            if member.member_id == member_id:
+                print(f"Member ID: {member.member_id}")
+                print(f"Name: {member.name}")
+                if isinstance(member, SingleClubMember):
+                    print(f"Club: {member.club.name}")
+                elif isinstance(member, MultiClubMember):
+                    print(f"Points: {member.points}")
+                return
+        print(f"Error: Member {member_id} not found.")
+
+    def check_in_member(self):
+        member_id = input("Enter member ID: ")
+        club_name = input("Enter club name: ")
+        club_address = input("Enter club address: ")
+        club = Club(club_name, club_address)
+        for member in self.members:
+            if member.member_id == member_id:
+                member.check_in(club)
+                return
+        print(f"Error: Member {member_id} not found.")
+
